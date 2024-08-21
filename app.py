a726c8a035c323fb54be83e7fba2e747b2f50be8
Scottcito <125676835+Scottcito@users.noreply.github.com>
2024-08-21 12:35:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 # Configurar el cliente de S3 usando credenciales del entorno
 s3 = boto3.client('s3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
 )
 
 BUCKET_NAME = 'modeloprueba'
