a9f1e9f9de7ca305c17002ed987c1770a1c9d502
Scottcito <125676835+Scottcito@users.noreply.github.com>
2024-09-14 23:13:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,9 @@ def predict_video():
         video_path = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4').name
         file.save(video_path)
 
-        labels = []
+        # Diccionario para contar las etiquetas
+        label_counts = {}
+        frequency_threshold = 3  # Umbral de frecuencia para filtrar etiquetas
 
         # Leer el video frame por frame
         cap = cv2.VideoCapture(video_path)
@@ -101,22 +103,23 @@ def predict_video():
             # Realizar la inferencia con el segundo modelo en cada frame
             results = model_2(frame)
 
-            # Procesar los resultados y agregar los labels detectados
+            # Procesar los resultados y contar los labels detectados
             for result in results:
                 for box in result.boxes:
                     label = model_2.names[int(box.cls)]
-                    if label not in labels:
-                        labels.append(label)
-
-            # Puedes detener el proceso si ya tienes suficientes labels
-            if labels:
-                break
+                    if label in label_counts:
+                        label_counts[label] += 1
+                    else:
+                        label_counts[label] = 1
 
         cap.release()
 
+        # Filtrar labels que superan el umbral de frecuencia
+        filtered_labels = [label for label, count in label_counts.items() if count >= frequency_threshold]
+
         # Devolver los labels detectados
-        logging.info(f"Predicción con video realizada con éxito. Labels: {labels}")
-        return jsonify({"data": {"labels": labels}})
+        logging.info(f"Predicción con video realizada con éxito. Labels: {filtered_labels}")
+        return jsonify({"data": {"labels": filtered_labels}})
 
     except Exception as e:
         logging.error(f"Error durante la predicción con el video: {e}", exc_info=True)
